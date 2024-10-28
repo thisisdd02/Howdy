97608bfda1c7585a70589969722d72bf90a86a70
divya dharshini <148539196+thisisdd02@users.noreply.github.com>
2024-10-28 15:48:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import { ConnectDB } from './config/db.js';
 import router from './route/routes.js';
 import {app,server} from './socket/index.js'
+import path from 'path'
 
 app.use(cors({
     origin: process.env.FRONTEND_URL,
@@ -12,6 +13,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(cookieParser());
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 4000;
 
 app.get('/',(req,res)=>{
@@ -20,6 +22,12 @@ app.get('/',(req,res)=>{
 
 //Api endpoits
 app.use('/api',router)
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 ConnectDB().then(()=>{
     server.listen(PORT,()=>{
